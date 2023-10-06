ca74eb1c41cf33518d120522e846afbdbd02145c
lkmelberg <100446631+lkmelberg@users.noreply.github.com>
2023-10-06 13:09:16 +0200

    module 3 goddamn

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,23 @@
-import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import addProduct, { incrementProduct } from "./store/products/products";
+import React from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
 
 function App() {
-  const [productTitle, setProduct] = useState("");
-  const products = [...useSelector((state) => state.products)].sort((a, b) => {
-    return a.title.toLowerCase() > b.title.toLowerCase() ? 1 : -1;
-  });
-  const dispatch = useDispatch();
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    dispatch(addProduct(productTitle));
-    setProduct("");
-  };
+  const birds = useSelector((state) => state.birds);
 
   return (
     <div className="wrapper">
-      <h1>Product List</h1>
-      {/* <form onSubmit={handleSubmit}>
-        <label>
-          <p>Add Product</p>
-          <input
-            type="text"
-            onChange={(e) => setProduct(e.target.value)}
-            value={productTitle}
-          />
-        </label>
-        <div>
-          <button type="submit">Add</button>
-        </div>
-      </form> */}
-      {/* <ul>
-        {products.map((product) => (
-          <li key={product.title}>
-            <h3>{product.title}</h3>
-            <div>
-              Views: {product.price}
-              <button onClick={() => dispatch(incrementProduct(product.title))}>
-                <span role="img" aria-label="add">
-                  ➕
-                </span>
-              </button>
-            </div>
+      <h1>Bird List</h1>
+      <ul>
+        {birds.map((bird) => (
+          <li key={bird.name}>
+            <h3>{bird.name}</h3>
+            <div>Views: {bird.views}</div>
           </li>
         ))}
-      </ul> */}
+      </ul>
     </div>
   );
-  console.log("works");
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { configureStore } from "@reduxjs/toolkit";
+
 import { Provider } from "react-redux";
-import productApp from "./store/products/products";
+import { createStore } from "zustand";
+const store = createStore(() => ({
+  birds: [
+    {
+      name: "robin",
+      views: 1,
+    },
+  ],
+}));
 
-// Import the Provider from `react-redux`
-
-const store = configureStore(productApp);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-// Wrap <App /> with our react-redux Provider, passing in the store
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
